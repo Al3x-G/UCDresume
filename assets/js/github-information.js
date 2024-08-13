2388a3c34ae6a911eb5594bb1a127a02c073072f
Alex Gordon <alexg1991@outlook.com>
2024-08-13 18:57:34 +0000

    complete second lesson on github APIs, code not tested

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -10,4 +10,34 @@ function fetchGitHubInformation(event) {
         `<div id="loader">
             <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
-} /* notice the use of back ticks, this section inputs a loader when text has been inputted */
+/* notice the use of back ticks (template literal), this section inputs a loader when text has been inputted */
+
+/********************************************************************************************* */
+
+/*below is a jquery promise, when: something has finished happening, then: do something else */
+/*
+
+when: we have got a response from github API
+
+then: function to display it in the gh-user-data div
+      unless we get an error!
+
+*/
+$.when(
+    $.getJSON(`https://api.github.com/users/${username}`)
+).then(
+    function(response) {
+        var userData = response;
+        $("#gh-user-data").html(userInformationHTML(userData));
+    },
+    function(errorResponse) {
+        if (errorResponse.status === 404) {
+            $("#gh-user-data").html(
+                `<h2>No info found for user ${username}</h2>`);
+        } else {
+            console.log(errorResponse);
+            $("#gh-user-data").html(
+                `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+        }
+    });
+}
