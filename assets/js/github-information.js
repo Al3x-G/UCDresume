e91a90e1bbeb0cd11eb5933be78978ca8d09acbc
Alex Gordon <alexg1991@outlook.com>
2024-08-13 19:40:14 +0000

    Add code to deal with github api throttling, display message with a wait time

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -82,6 +82,10 @@ $.when(
         if (errorResponse.status === 404) {
             $("#gh-user-data").html(
                 `<h2>No info found for user ${username}</h2>`);
+        } else if (errorResponse.status === 403) {
+                var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset') * 1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+        /*404 is error (not found, render why), 403 is forbidden (cant do that, so render a message with a wait time) */    
         } else {
             console.log(errorResponse);
             $("#gh-user-data").html(
