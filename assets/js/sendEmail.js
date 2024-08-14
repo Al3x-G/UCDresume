120c6d430eaadcb1fa7eab2232d11d1d8fe7bdeb
Alex Gordon <alexg1991@outlook.com>
2024-08-14 18:52:11 +0000

    Add function to js file to set up emailjs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,18 @@
+// sevice id is gmail, template id is rosie (all set up on emailjs website)
 
+function sendMail(contactForm) {
+    emailjs.send("gmail", "rosie", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
